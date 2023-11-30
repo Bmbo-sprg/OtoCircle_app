f84ce278662c8c476b6042a1a5725578be8627a5
OH Atsunobu <oh.atsunobu.47j@st.kyoto-u.ac.jp>
2023-12-01 04:47:20 +0900

    update musics controller api

--- app/controllers/api/v1/musics_controller.rb
+++ app/controllers/api/v1/musics_controller.rb
@@ -2,14 +2,14 @@ class Api::V1::MusicsController < ApplicationController
   before_action :set_music, only: [:show, :destroy]
 
   def index
-    @musics = Music.all.order(created_at: :desc)
-    render json: @musics, status: :ok
+    @musics = Music.all.order(created_at: :desc).includes(:composer)
+    render json: @musics.as_json(include: [:composer]), status: :ok
   end
 
   def create
     @music = Music.new(music_params)
     if @music.save
-      render json: @music, status: :created, location: @music
+      render json: @music, status: :created
     else
       render json: @music.errors, status: :unprocessable_entity
     end
